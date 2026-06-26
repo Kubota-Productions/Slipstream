667b05e98903b83546dccf6ac3ea02a6fc113195
Kubota-Productions <124377538+Kubota-Productions@users.noreply.github.com>
2026-06-26 18:12:47 +0900

    update, changed car code a little

--- Assets/Player/vehicles/car.gd
+++ Assets/Player/vehicles/car.gd
@@ -1,25 +1,29 @@
 extends VehicleBody3D
 
-const  MAX_STEER = 0.8
+const  MAX_STEER = 0.4
 const  ENGINE_POWER = 300
 const  DRIFT = 0.0
+
+var speed = linear_velocity.length()
+var steer_multiplier = lerp(1.0, 0.1, clamp(speed / 90.0, 0.0, 0.01))
+var max_steer = MAX_STEER * steer_multiplier
 
 @onready var camera_pivot = $CameraPivot
 @onready var camera_3d = $CameraPivot/Camera3D
 @onready var reverse_camera = $CameraPivot/ReverseCamera
 @onready 	var active = "player"
 
-var look_at
+var looking_at
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	look_at = global_position
+	looking_at = global_position
 	
 	camera_3d.current = false
 	reverse_camera.current = false
 
-func _input(event):
+func _input(_event):
 		if Input.is_action_pressed("quit"):
 			get_tree().quit()
 
@@ -32,9 +36,9 @@ func _physics_process(delta):
 		
 		camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
 		camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
-		look_at = look_at.lerp(global_position + linear_velocity, delta * 5.0)
-		camera_3d.look_at(look_at)
-		reverse_camera.look_at(look_at)
+		looking_at = looking_at.lerp(global_position + linear_velocity, delta * 5.0)
+		camera_3d.look_at(looking_at)
+		reverse_camera.look_at(looking_at)
 		_check_camera_switch()
 		
 func _check_camera_switch():
